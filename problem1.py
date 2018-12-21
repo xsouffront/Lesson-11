0b090584d6e163f1752585b45f70edf15ec62e26
xsouffront <43756495+xsouffront@users.noreply.github.com>
2018-12-21 09:56:07 -0500

    Add files via upload

---
+++ problem1.py
@@ -0,0 +1,10 @@
+print('-'*88)
+print('Is it cold ? ')
+print()
+temperature= input('Temperature in Fahrenheit: ')
+temperature= int(temperature)
+
+if temperature>=32:
+	print(' It is cold, bundle up!')
+else:
+	print('It is not cold, have a nice day!')
---
+++ problem2.py
@@ -0,0 +1,10 @@
+print('-'*88)
+print('Narrative Bot')
+print()
+Name= input('Student Name:  ')
+grade= input('Grade :  ')
+grade=int(grade)
+if grade>=65:
+	print('You have excelled, congratulations you have passed!')
+else:
+	print('Unfortantely you have not passed, try again.')
